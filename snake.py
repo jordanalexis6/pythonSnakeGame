1f6700294f0629ae5eb64957ff88a18a1f268c77
Jordan Neill <jordanneill@Jordans-MacBook-Pro.local>
2021-08-05 20:56:02 -0500

    setting up the screen

--- snake.py
+++ snake.py
@@ -2,3 +2,13 @@
 import turtle
 import time
 import random
+
+# Screen
+wn = turtle.Screen()
+wn.title("Simple Snake Game by @jordanalexis6")
+wn.bgcolor("#28113d")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+wn.mainloop()
