2b4525e928502a1d3f3506ea52788e512888a5dd
Jordan Neill <jordanneill@Jordans-MacBook-Pro.local>
2021-08-05 21:07:01 -0500

    added snake head & made it appear on screen

--- snake.py
+++ snake.py
@@ -3,12 +3,25 @@ import turtle
 import time
 import random
 
-# Screen
+# main screen
 wn = turtle.Screen()
 wn.title("Simple Snake Game by @jordanalexis6")
 wn.bgcolor("#28113d")
 wn.setup(width=600, height=600)
 wn.tracer(0)
+
+# snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# main game loop
+while True:
+    wn.update()
 
 
 wn.mainloop()
